b1935955e441bc4270510ee27c976809a53956ff
sanukumar <sanukumar1138@gmail.com>
2024-02-14 04:37:07 +0530

    updated create post

---
+++ GetPosts/index.js
@@ -0,0 +1,20 @@
+const azure = require("azure-storage");
+const { queryEntities } = require("../services/tableService");
+
+module.exports = async (context, req) => {
+  try {
+    const blog = context.bindingData.blog;
+    let query = new azure.TableQuery().where("Partition eq ?", blog);
+
+    const result = await queryEntities("Posts", query);
+
+    context.res = {
+      body: result,
+    };
+  } catch (error) {
+    context.res = {
+      status: 500,
+      body: error.message,
+    };
+  }
+};
---
+++ services/tableService.js
@@ -0,0 +1,40 @@
+const azure = require("azure-storage");
+
+const tableSvc = azure.createTableService(
+  "restapinode",
+  process.env.AZURE_STORAGE_ACCESS_KEY
+);
+
+const insertEntity = (tableName, entity) => {
+  return new Promise((resolve, reject) => {
+    tableSvc.insertEntity(
+      tableName,
+      entity,
+      { echoContent: true, payloadFormat: "application/json;odata=nometadata" },
+      (error, result, response) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(response.body);
+      }
+    );
+  });
+};
+
+const queryEntities = (tableName, query) => {
+  return new Promise((resolve, reject) => {
+    tableSvc.queryEntities(
+      tableName,
+      query,
+      { payloadFormat: "application/json;odata=nometadata" },
+      (error, result, response) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(response.body);
+      }
+    );
+  });
+};
+
+exports.insertEntity = insertEntity;
